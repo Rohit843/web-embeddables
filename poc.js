9208bedc64a89e2f49a052f92dada0acac0f4367
Rohit843 <94066068+Rohit843@users.noreply.github.com>
2025-05-08 14:17:43 +0530

    Update poc.js

--- poc.js
+++ poc.js
@@ -34,9 +34,12 @@ function getTokenCallBack() {
 	var idToken = access_token;
 	return Promise.resolve(idToken);
 }
-await login();
- const elRef1 =  document.getElementById("cmp1");
-	  setProperties(elRef1, {"string2": "Rohit Kumar Dogra","json1":[{"label":"Software Developer","clickable":false,"id":"Software Developer"}],"json2":[{"label":"Mobile Phone","value":"+91-8968422868","clickable":true,"id":"mobile-link"},{"label":"Email","value":"rohit.kumardogra@servicenow.com","clickable":true,"id":"email-link"},{"label":"City","value":"Bangalore"},{"label":"State","value":"Karnatka"}]});
+
+login().then(()=>{
+  const elRef1 =  document.getElementById("cmp1");
+  setProperties(elRef1, {"string2": "Rohit Kumar Dogra","json1":[{"label":"Software Developer","clickable":false,"id":"Software Developer"}],"json2":[{"label":"Mobile Phone","value":"+91-8968422868","clickable":true,"id":"mobile-link"},{"label":"Email","value":"rohit.kumardogra@servicenow.com","clickable":true,"id":"email-link"},{"label":"City","value":"Bangalore"},{"label":"State","value":"Karnatka"}]});
+  
+});
 	
 // await logout();
 
